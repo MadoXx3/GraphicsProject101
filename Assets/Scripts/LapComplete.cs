b3d7f11b2ed6b6249f57d092841ff111d9919220
MadoXx3 <mohamed.hosean@yahoo.com>
2018-04-29 08:10:09 +0200

    CounrDown And Record

--- Assets/Scripts/LapComplete.cs
+++ Assets/Scripts/LapComplete.cs
@@ -30,6 +30,10 @@ public class LapComplete : MonoBehaviour {
 
 		MilliDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MilliCount;
 
+		PlayerPrefs.SetInt ("MinSave", LapTimeManager.MinuteCount);
+		PlayerPrefs.SetInt ("SecSave", LapTimeManager.SecondCount);
+		PlayerPrefs.SetFloat ("MilliSave", LapTimeManager.MilliCount);
+
 		LapTimeManager.MinuteCount = 0;
 		LapTimeManager.SecondCount = 0;
 		LapTimeManager.MilliCount = 0;
